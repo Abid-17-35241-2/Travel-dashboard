d1d6666abd124c3c60d8f480a2985374b59f070a
Abid-17-35241-2 <abidhossain403@gmail.com>
2023-09-25 10:23:08 +0600

    Travel-Dashboard

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+  "use strict";
+  $("#datepicker").datepicker( {
+	firstDay: 1,
+	// changeMonth: true,
+	// changeYear: true,
+	prevText: '<i class="fa fa-fw fa-angle-left"></i>',
+	  nextText: '<i class="fa fa-fw fa-angle-right"></i>',
+	
+	
+  });
+ 
+ 
+})(jQuery);
